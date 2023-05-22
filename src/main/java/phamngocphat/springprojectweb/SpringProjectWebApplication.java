2877ac4d528fb5759be619d612f9d2d180e99d3c
Pham Phat <phamphat261023@gmail.com>
2023-05-22 11:06:35 +0700

    spring-project-web

---
+++ src/main/java/phamngocphat/springprojectweb/SpringProjectWebApplication.java
@@ -0,0 +1,14 @@
+package phamngocphat.springprojectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectWebApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
